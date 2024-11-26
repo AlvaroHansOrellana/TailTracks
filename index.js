125a55599b0c7b66a0e65f3feb231d3b4b71e49f
Hans <alvarohansorellana@gmail.com>
2024-11-26 16:44:52 +0100

    routes registered

--- index.js
+++ index.js
@@ -2,44 +2,43 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const path = require('path');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swagger.json');
-const morgan = require('./middlewares/requestLogger');
+const db = require('./config/db'); // Conexión a la base de datos
+// ! Import de middelwares
+const requestLogger = require('./middlewares/requestLogger');
 const manage404 = require('./middlewares/manage404');
 const errorHandler = require('./middlewares/errorHandler');
+const validationMiddleware = require('./middlewares/validationMiddleware')
 
-dotenv.config(); // Variables de entorno
+const routes = require('./routes'); // Archivo central de rutas para tenerlo bien organizadito bb
+
+// ! Variables de entorno
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middlewares
+// Base de datos: Ejemplo de inicialización
+db.query(
+    "INSERT INTO Usuario (nombre, email, contraseña, ubicacion, telefono) VALUES ('Juan Pérez', 'pedros@example.com', '123456', 'Madrid', '123456789')"
+)
+    .then(() => console.log('Datos insertados correctamente'))
+    .catch((err) => console.error('Error al insertar datos:', err));
+
+// ! Uso de los Middlewares
 app.use(express.json()); // Parseo de JSON
 app.use(express.urlencoded({ extended: true })); // Parseo de datos URL-encoded
 app.use(cookieParser()); // Parseo de cookies
-app.use(morgan(':method :url :status - :response-time ms :body')); // Logs con Morgan
+app.use(requestLogger);
 app.use('/public', express.static(path.join(__dirname, 'public'))); // Archivos estáticos
 
-// Swagger (Documentación)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// ! Registrar Rutas
+app.use('/api', routes);
 
-// Importar Rutas
-const usuarioRoutes = require('./routes/usuarioRoutes');
-const paseoRoutes = require('./routes/paseoRoutes');
-const perroRoutes = require('./routes/perroRoutes');
-const reservaRoutes = require('./routes/reservaRoutes');
-
-// Registrar Rutas
-app.use('/api/usuarios', usuarioRoutes);
-app.use('/api/paseos', paseoRoutes);
-app.use('/api/perros', perroRoutes);
-app.use('/api/reservas', reservaRoutes);
-
-// Middlewares para manejo de errores
+// ! Manejo de errores
 app.use('*', manage404); // Manejo de rutas no encontradas
 app.use(errorHandler); // Manejo global de errores
 
-// Iniciar Servidor
+// ! Iniciar Servidor
 app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`🚀 Servidor escuchando en http://localhost:${port}`);
 });
