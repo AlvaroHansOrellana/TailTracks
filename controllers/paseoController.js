1510aa54182395c51a9e0800dea04cf914e598b7
Hans <alvarohansorellana@gmail.com>
2024-11-26 18:38:36 +0100

    added validation2

--- controllers/paseoController.js
+++ controllers/paseoController.js
@@ -1,32 +1,33 @@
 const paseoModel = require('../models/paseo');
+const CustomError = require('../utils/customError');
 
 // Obtener todos los paseos
-const getAllWalks = async (req, res) => {
+const getAllWalks = async (req, res, next) => {
     try {
         const walks = await paseoModel.getAllWalks();
         res.json(walks);
     } catch (err) {
-        res.status(500).json({ message: 'Error al obtener los paseos', error: err });
+        next(new CustomError('Error al obtener los paseos', 500));
     }
 };
 
 // Crear un paseo
-const createWalk = async (req, res) => {
+const createWalk = async (req, res, next) => {
     try {
         const walk = await paseoModel.createWalk(req.body);
         res.status(201).json(walk);
     } catch (err) {
-        res.status(500).json({ message: 'Error al crear el paseo', error: err });
+        next(err);
     }
 };
 
 // Eliminar un paseo
-const deleteWalk = async (req, res) => {
+const deleteWalk = async (req, res, next) => {
     try {
         await paseoModel.deleteWalk(req.params.id_paseo);
         res.status(204).send();
     } catch (err) {
-        res.status(500).json({ message: 'Error al eliminar el paseo', error: err });
+        next(err);
     }
 };
 
