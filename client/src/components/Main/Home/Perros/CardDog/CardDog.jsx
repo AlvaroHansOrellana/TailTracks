b25cf4f2a4bbfe1b612bb5c37d6959434ff83e30
Hans <alvarohansorellana@gmail.com>
2024-12-01 20:03:51 +0100

    fixed images display

--- client/src/components/Main/Home/Perros/CardDog/CardDog.jsx
+++ client/src/components/Main/Home/Perros/CardDog/CardDog.jsx
@@ -5,12 +5,14 @@ import "./CardDog.scss";
 const CardDog = ({ perro }) => {
   return (
     <div className="card-dog">
-      <Link to={`/perros/${perro.nombre}`} className="dog-link"> {/* Redirige usando el nombre */}
-        <img
-          src={perro.foto || "placeholder.jpg"}
-          alt={`Imagen de ${perro.nombre}`}
-          className="dog-image"
-        />
+      <Link to={`/perros/${perro.nombre}`} className="dog-link">
+        <div className="image-wrapper">
+          <img
+            src={perro.foto || "placeholder.jpg"}
+            alt={`Imagen de ${perro.nombre}`}
+            className="dog-image"
+          />
+        </div>
         <h2>{perro.nombre}</h2>
       </Link>
     </div>
